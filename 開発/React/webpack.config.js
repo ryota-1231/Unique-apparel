256385409fc2b8c4dda8bee32c840cdbdd554e63
ryota-1231 <rrrrr.1231@gmail.com>
2020-03-01 14:24:36 +0900

    js rules fix
    ## What
    - JSのコード規約にのっとりコードを修正
    
    ## Why
    - 角ソースを統一しメンテナンスしやすくするため

--- 開発/React/webpack.config.js
+++ 開発/React/webpack.config.js
@@ -1,31 +1,31 @@
 module.exports = {
   entry: {
-    app: "./src/index.js"
+    app: './src/index.js'
   },
   output: {
     path: __dirname + '/public/js',
-    filename: "[name].js"
+    filename: '[name].js'
   },
-    devServer: {
+  devServer: {
     contentBase: __dirname + '/public',
     port: 8080,
     publicPath: '/js/'
   },
-  devtool: "eval-source-map",
+  devtool: 'eval-source-map',
   mode: 'development',
   module: {
     rules: [{
       test: /\.js$/,
-      enforce: "pre",
+      enforce: 'pre',
       exclude: /node_modules/,
-      loader: "eslint-loader"
+      loader: 'eslint-loader'
     }, {
       test: /\.css$/,
-      loader: ["style-loader","css-loader"]
+      loader: ['style-loader', 'css-loader']
     }, {
       test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel-loader'
-     }]
+    }]
   }
-};
+}
