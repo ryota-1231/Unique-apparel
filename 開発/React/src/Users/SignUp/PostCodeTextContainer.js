8efc543d641b08a8fb6234d7cc3c22ccb8971560
ryota-1231 <rrrrr.1231@gmail.com>
2020-03-03 23:10:47 +0900

    postcode css-fix

--- 開発/React/src/Users/SignUp/PostCodeTextContainer.js
+++ 開発/React/src/Users/SignUp/PostCodeTextContainer.js
@@ -16,8 +16,8 @@ export default function BasicTextFields() {
   const classes = useStyles();
 
   return (
-    <form id='address-form' className={classes.root} noValidate autoComplete="off">
-      <TextField id="address-post-code" label="郵便番号" variant="outlined" />
+    <form id='postcode-form' className={classes.root} noValidate autoComplete="off">
+      <TextField id="postcode" label="郵便番号" variant="outlined" />
       <Button id='address-auto-input-interface' variant="contained" color="primary">住所自動入力</Button>
       <Link href='https://www.post.japanpost.jp/zipcode/index.html'><Button id='post-code-search-interface' color="primary">郵便番号を検索</Button></Link>
     </form>
