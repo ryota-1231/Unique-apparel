5e4c7974730a7008ae2be9e948c1954a1f1cb957
ryota-1231 <rrrrr.1231@gmail.com>
2020-03-04 12:45:24 +0900

    standard fix

--- 開発/React/src/Users/Login/main.js
+++ 開発/React/src/Users/Login/main.js
@@ -3,7 +3,6 @@ import React from 'react'
 import './main.css'
 import MainContainer from './MainContainer'
 
-
 class UsersLogin extends React.Component {
   render () {
     return (
--- 開発/React/src/Users/SignUp/PhoneNumberTextField.js
+++ 開発/React/src/Users/SignUp/PhoneNumberTextField.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
 
 const useStyles = makeStyles(theme => ({
   root: {
     '& > *': {
-      margin: theme.spacing(1),
-    },
-  },
-}));
+      margin: theme.spacing(1)
+    }
+  }
+}))
 
-export default function BasicTextFields() {
-  const classes = useStyles();
+export default function BasicTextFields () {
+  const classes = useStyles()
 
   return (
-    <form id='phone-number-form' className={classes.root} noValidate autoComplete="off">
-      <TextField id="phone-number-input" label="電話番号" style={{ margin: 8 }} margin="normal"  helperText="ハイフンなしで記入してください" variant="outlined" />
+    <form id='phone-number-form' className={classes.root} noValidate autoComplete='off'>
+      <TextField id='phone-number-input' label='電話番号' style={{ margin: 8 }} margin='normal' helperText='ハイフンなしで記入してください' variant='outlined' />
     </form>
-  );
+  )
 }
