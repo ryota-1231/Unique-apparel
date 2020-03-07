f663c2c2e19804be8e501160da89ce96e8773afa
ryota-1231 <rrrrr.1231@gmail.com>
2020-03-07 19:43:55 +0900

    phone_number_area

--- 開発/React/src/Companies/SignUp/PhoneNumberTextField.js
+++ 開発/React/src/Companies/SignUp/PhoneNumberTextField.js
@@ -15,7 +15,7 @@ export default function BasicTextFields () {
 
   return (
     <form id='phone-number-form' className={classes.root} noValidate autoComplete='off'>
-      <TextField id='phone-number-input' label='電話番号' style={{ margin: 8 }} margin='normal' helperText='ハイフンなしで記入してください' variant='outlined' />
+      <TextField id='phone-number-input' label='電話番号' style={{ margin: 8 }} margin='normal' helperText='ハイフンなしで記入してください' variant='outlined' size='small' />
     </form>
   )
 }
