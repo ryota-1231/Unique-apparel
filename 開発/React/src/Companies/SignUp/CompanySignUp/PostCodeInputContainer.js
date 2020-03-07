0bc67b0d740095357729b40528ead2f6a03f0443
ryota-1231 <rrrrr.1231@gmail.com>
2020-03-07 22:43:48 +0900

    address_area

---
+++ 開発/React/src/Companies/SignUp/CompanySignUp/PostCodeInputContainer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import AddressGetApi from './AddressGetApi'
+
+class PostCodeInputContainer extends React.Component {
+  render () {
+    return (
+      <div className='postcode-area'>
+        <div className='postcode-title-area'>
+          <h4 className='address-title'>{this.props.title}</h4>
+        </div>
+        <AddressGetApi />
+      </div>
+    )
+  }
+}
+
+PostCodeInputContainer .propTypes = {
+  title: PropTypes.string,
+}
+
+
+export default PostCodeInputContainer
