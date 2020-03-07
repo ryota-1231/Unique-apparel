50db8a78b2026e8413fdc9c93699b70dbd9ddab4
ryota-1231 <rrrrr.1231@gmail.com>
2020-03-07 22:45:36 +0900

    main

--- 開発/React/src/Companies/SignUp/MainContainer.js
+++ 開発/React/src/Companies/SignUp/MainContainer.js
@@ -4,12 +4,8 @@ import Card from '@material-ui/core/Card'
 import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
 import Button from '@material-ui/core/Button'
-import BusinessTypeContainer from './BusinessTypeContainer'
-import CompanyNameContainer from './CompanyNameContainer'
-import PostCodeInputContainer from './PostCodeInputContainer'
-import PhoneNumberInputContainer from './PhoneNumberInputContainer'
-import NameInputContainer from './NameInputContainer'
-import NameKanaInputContainer from './NameKanaInputContainer'
+import CompanySignUp from './CompanySignUp'
+import ShopSignUp from './ShopSignUp'
 
 const useStyles = makeStyles({
   root: {
@@ -32,37 +28,42 @@ export default function SimpleCard () {
   const classes = useStyles()
 
   return (
-    <Card id='signup-area' className={classes.root}>
-      <CardContent>
-        <div className='contents'>
-          <div className='contents-title-area'>
-            <h3 className='contents-title'>会社情報</h3>
+    <div className='company-signup-wrapper'>
+      <Card id='signup-area' className={classes.root}>
+        <CardContent>
+          <div className='contents'>
+            <div id='check-list-area'>
+              <h3 className='check-list-title'>※ご確認事項※</h3>
+              <p className='check-list-description'>
+                ・内裏での出品申込は承ることができません。出店希望者ご自身でお申込みください。<br></br>
+                ・本フォームの入力が完了した時点では、契約の成立にはなりません。
+              </p>
+            </div>
           </div>
-          <div id='check-list-area'>
-            <h3 className='check-list-title'>※ご確認事項※</h3>
-            <p className='check-list-description'>
-              ・内裏での出品申込は承ることができません。出店希望者ご自身でお申込みください。<br></br>
-              ・本フォームの入力が完了した時点では、契約の成立にはなりません。
-            </p>
-          </div>
-          {/* -------------事業形態------------- */}
-          <BusinessTypeContainer />
-          {/* -------------会社名入力------------- */}
-          <CompanyNameContainer />
-          {/* -------------住所入力------------- */}
-          <PostCodeInputContainer />
-          {/* -------------電話番号入力------------- */}
-          <PhoneNumberInputContainer />
-          {/* -------------代表者氏名入力------------- */}
-          <NameInputContainer />
-          {/* -------------代表者氏名（かな）入力------------- */}
-          <NameKanaInputContainer />
-        
-        </div>
-      </CardContent>
+        </CardContent>
+      </Card>
+      <div className='contents-title-area'>
+        <h3 className='contents-title'>会社情報</h3>
+      </div>
+      <Card id='signup-area' className={classes.root}>
+        <CardContent>
+          {/* -------------会社登録エリア------------- */}
+          <CompanySignUp />
+        </CardContent>
+      </Card>
+      <div className='contents-title-area'>
+        <h3 className='contents-title'>店舗運営責任者</h3>
+      </div>
+      <Card id='signup-area' className={classes.root}>
+        <CardContent>
+          {/* -------------店舗登録エリア------------- */}
+          <ShopSignUp />
+        </CardContent>
+      </Card>
+       {/* -------------登録ボタンエリア------------- */}
       <CardActions id='signup-btn-area'>
         <Button variant='contained' color='primary' size='large' id='user-register-btn'>会員登録する</Button>
       </CardActions>
-    </Card>
+    </div>
   )
 }
