3159a445143bb7460d1380cfc650e135fd31a550
ryota-1231 <rrrrr.1231@gmail.com>
2020-03-08 23:44:45 +0900

    story_install

---
+++ 開発/React/src/Atoms/button/stories/0-Welcome.stories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default {
+  title: 'Welcome',
+  component: h3Tag
+};
+
+export function h3Tag () {
+  return (
+    <h3>kkkkkkk</h3>
+  )
+}
+
+h3Tag.story = {
+  name: ' LikeButton',
+};
+
---
+++ 開発/React/src/Atoms/heading/h3.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const H3 = <h3>{name}</h3>;
+
+export default function h3Tag () {
+  return (
+    <H3 name={this.props.name}/>
+  )
+}
+
