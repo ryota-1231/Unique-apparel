188e0e88c9ac96f332ac152e5964e086fde03d6b
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-03-08 11:06:47 +0900

    process_cutout side_bottom.js

--- 開発/React/src/Items/purchase/side_area.js
+++ 開発/React/src/Items/purchase/side_area.js
@@ -1,30 +1,18 @@
-import React from 'react';
+import React from 'react'
 import Button from '@material-ui/core/Button'
-import Link from '@material-ui/core/Link';
+import PurchaseTotal from './parts_of_purchase/purchase_total.js'
+import EachPrice from './parts_of_purchase/each_price.js'
 
-export default function PurchaseFixing() {
+export default function PurchaseFixing () {
   return (
     <aside>
-      <Button variant="contained" color="primary" disableElevation className="purchase_button">
+      <Button variant='contained' color='primary' disableElevation className='purchase_button'>
         購入確定する
       </Button>
-      <section className="total_area">
-        <section>
-          <p>商品代</p>
-          <p>クーポン</p>
-          <p>送料</p>
-          <p>支払い手数料</p>
-          <p className="total">合計(税込み)</p>
-          <Link href="#">返品について</Link>
-        </section>
-        <section>
-          <p className="each_price">¥ 4500</p>
-          <p className="each_price">¥ 0</p>
-          <p className="each_price">¥ 210</p>
-          <p className="each_price">¥ 0</p>
-          <p className="total_price">¥ 4710</p>
-        </section>
+      <section className='purchase_total_area'>
+        <PurchaseTotal />
+        <EachPrice />
       </section>
     </aside>
-  );
+  )
 }
