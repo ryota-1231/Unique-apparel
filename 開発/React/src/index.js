f3f1fac518a3a2818fc8a51c1a07a319e4bc58c9
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-03-08 18:30:39 +0900

    update index.js

--- 開発/React/src/index.js
+++ 開発/React/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import ItemIndex from './Items/index/main'
+// import ItemIndex from './Items/index/main'
 import './reset.css'
 // import OrderComplete from './Orders/complete/main'
 // import UsersSignUp from './Users/SignUp/main'
 // import UsersLogin from './Users/Login/main'
+import ListingNew from './Items/listings/main'
 ReactDOM.render(
   // <UsersSignUp />,
 //   <UsersLogin />,
   // <OrderComplete />,
-    <ItemIndex />,
+  // <ItemIndex />,
+  <ListingNew />,
   document.getElementById('root')
 )
