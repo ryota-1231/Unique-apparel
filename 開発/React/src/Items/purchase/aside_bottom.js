f282eef09725701e3a9f9f4d2b8d5970f6f001ce
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-03-08 11:04:45 +0900

    process_cutout aside_bottom.js

--- 開発/React/src/Items/purchase/aside_bottom.js
+++ 開発/React/src/Items/purchase/aside_bottom.js
@@ -1,41 +1,25 @@
-import React from 'react';
-import Button from '@material-ui/core/Button'
-import Items from './items.js';
+import React from 'react'
+import Items from './parts_of_purchase/items.js'
+import OrderItem from './parts_of_purchase/order_item.js'
+import ChangeButton from './parts_of_purchase/change_button.js'
 
-export default function PurchaseItemInformation() {
+export default function PurchaseItemInformation () {
   return (
-    <aside className="aside_bottom">
+    <aside className='aside_of_your_order'>
       <h2>お届けの商品</h2>
-
-      <section className="section_top">
-        <h3>お届け日</h3>
-        <div>
-          <p>指定無し:2月25日~2月27日発送予定</p>
-          <p>日時指定:午前中</p>
-          <p>即日配送:最短2月24日(月)午前中</p>
-        </div>
-        <div>
-          <p>¥ 0</p>
-          <p>¥ 0</p>
-          <p>¥ 350</p>
-        </div>
-        <div className="button_area">
-          <Button variant="outlined" color="primary">変更</Button>
-        </div>
-      </section>
-
-      <section className="section_image">
+      {/* order_itemコンポーネントの呼び出し */}
+      <OrderItem />
+      <section className='section_image_area'>
         <h3>商品</h3>
+        {/* itemsコンポーネントの呼び出し */}
         <Items />
-        <div className="button_area">
-          <Button variant="outlined" color="primary">変更</Button>
-        </div>
+        {/* change_buttonコンポーネントの呼び出し */}
+        <ChangeButton />
       </section>
-
-      <section className="section_bottom">
+      <section className='sipping_fee_area'>
         <h3>送料</h3>
-        <p className="payment_amount">¥ 210</p>
+        <p className='payment_amount'>¥ 210</p>
       </section>
     </aside>
-  );
+  )
 }
