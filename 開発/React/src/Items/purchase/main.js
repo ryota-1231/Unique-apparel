8b5d6e55863cfff2204c5a2807518a36c7496c40
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-03-08 11:06:11 +0900

    standard --fix main.js

--- 開発/React/src/Items/purchase/main.js
+++ 開発/React/src/Items/purchase/main.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react'
 import './main.css'
-import PurchaseConfirmation from './aside_top';
-import PurchaseItemInformation from './aside_bottom';
-import PurchaseFixing from './side_area';
+import PurchaseConfirmation from './aside_top'
+import PurchaseItemInformation from './aside_bottom'
+import PurchaseFixing from './side_area'
 
 class ItemPurchase extends Component {
   render () {
     return (
-      <div className="contents">
-        <div className="main">
+      <div className='contents'>
+        <div className='main'>
           {/* aside_topコンポーネントの呼び出し */}
           <PurchaseConfirmation />
           {/* aside_bottomコンポーネントの呼び出し */}
           <PurchaseItemInformation />
         </div>
-        <div className="side">
+        <div className='side'>
+          {/* side_areaコンポーネントの呼び出し */}
           <PurchaseFixing />
         </div>
       </div>
@@ -22,4 +23,4 @@ class ItemPurchase extends Component {
   }
 }
 
-export default ItemPurchase;
+export default ItemPurchase
