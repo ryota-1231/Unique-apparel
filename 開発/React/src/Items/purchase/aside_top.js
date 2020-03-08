04f4604bb8814bde92520822eb71f6b95e946fa7
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-03-08 11:05:00 +0900

    process_cutout aside_top.js

--- 開発/React/src/Items/purchase/aside_top.js
+++ 開発/React/src/Items/purchase/aside_top.js
@@ -1,39 +1,18 @@
-import React from 'react';
-import Button from '@material-ui/core/Button'
+import React from 'react'
+import OrderConfirmation from './parts_of_purchase/order_confirmation.js'
+import OrderPayment from './parts_of_purchase/order_payment.js'
+import OrderCoupon from './parts_of_purchase/order_coupon.js'
 
-export default function PurchaseConfirmation() {
+export default function PurchaseConfirmation () {
   return (
-    <aside className="aside_top">
+    <aside className='aside_of_your_personal_information'>
       <h1>注文内容の確認</h1>
-
-      <section className="section_top">
-        <h3>お届け先</h3>
-        <div>
-          <p>チームぱぱ様</p>
-          <p>住所:〒1510001 東京都渋谷区千駄ヶ谷123-4 コーポ原宿3F</p>
-          <p>電話:03-1234-5678</p>
-        </div>
-        <div className="button_area">
-          <Button variant="outlined" color="primary">変更</Button>
-        </div>
-      </section>
-
-      <section className="section_middle">
-        <h3>支払い方法</h3>
-        <p className="payment_type">クレジットカード</p>
-        <p className="payment_amount">¥ 0</p>
-        <div className="button_area">
-          <Button variant="outlined" color="primary">変更</Button>
-        </div>
-      </section>
-
-      <section className="section_bottom">
-        <p className="payment_type">クーポン</p>
-        <p className="payment_amount">ご利用可能クーポン:5%オフ</p>
-        <div className="button_area">
-          <Button variant="outlined">✓</Button>
-        </div>
-      </section>
+      {/* order_informationコンポーネントの呼び出し */}
+      <OrderConfirmation />
+      {/* order_paymentコンポーネントの呼び出し */}
+      <OrderPayment />
+      {/* order_couponコンポーネントの呼び出し */}
+      <OrderCoupon />
     </aside>
-  );
+  )
 }
