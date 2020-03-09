d1dd783545b2bba05f7c71b6621a9b6ad52e444e
ryota-1231 <rrrrr.1231@gmail.com>
2020-03-09 21:37:05 +0900

    pages_file_edit

--- 開発/React/src/Companies/SignUp/CompanySignUp.js
+++ 開発/React/src/Companies/SignUp/CompanySignUp.js
@@ -1,26 +1,24 @@
 import React from 'react'
-import RadioField from '../../Molecules/RadioField'
-import CompanyNameContainer from './CompanySignUp/CompanyNameContainer'
+import RadioField from '../../Molecules/RadioArea'
 import PostCodeInputContainer from './CompanySignUp/PostCodeInputContainer'
 import PhoneNumberInputContainer from './CompanySignUp/PhoneNumberInputContainer'
-import NameInputContainer from './CompanySignUp/NameInputContainer'
+import TextArea from '../../Molecules/TextArea'
 
 class CompanySignUp extends React.Component {
   render () {
     return (
       <div className='company-signup-area'>
         {/* -------------事業形態------------- */}
-        <RadioField name='事業形態' label={['法人', '個人事業主']} />
+        <RadioField title='事業形態' label={['法人', '個人事業主']} size='small' />
         {/* -------------会社名入力------------- */}
-        <CompanyNameContainer title='会社名' label='正式名称' />
-        <CompanyNameContainer label='ふりがな' />
+        <TextArea title='会社名' label={['正式名称', 'ふりがな']} size='small'/>
         {/* -------------住所入力------------- */}
         <PostCodeInputContainer title='本社所在地' />
         {/* -------------電話番号入力------------- */}
         <PhoneNumberInputContainer label='代表電話番号' />
         {/* -------------代表者氏名入力------------- */}
-        <NameInputContainer title='代表者名' leftLabel='氏' rightLabel='名' />
-        <NameInputContainer title='ふりがな' leftLabel='氏（かな）' rightLabel='名（かな）' />
+        <TextArea title='代表者名' label={['氏','名']} size='small'/>
+        <TextArea title='ふりがな' label={['氏（かな）','名（かな）']} size='small'/>
       </div>
     )
   }
