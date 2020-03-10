60d93243328a6b81a46ccef159e9929542bc740e
ryota-1231 <54019059+ryota-1231@users.noreply.github.com>
2020-03-10 20:19:57 +0900

    fix
    コメントアウト削除

--- 開発/React/src/Atoms/input/stories/radioBtn.stories.js
+++ 開発/React/src/Atoms/input/stories/radioBtn.stories.js
@@ -36,27 +36,3 @@ export function RadioBtn () {
 RadioBtn.story = {
   name: 'RadioBtn'
 }
-
-// import React from 'react';
-
-// import { action } from '@storybook/addon-actions';
-// import { Button } from '@storybook/react/demo';
-
-// export default {
-//   title: 'w',
-//   component: Button,
-// };
-
-// export const Text = () => <Button onClick={action('click')}>Hello Button</Button>;
-
-// export const Emoji = () => (
-//   <Button onClick={action('clicked')}>
-//     <span role="img" aria-label="so cool">
-//       😀 😎 👍 💯
-//     </span>
-//   </Button>
-// );
-
-// // Emoji.story = {
-// //   name: 'with emoji',
-// // };
