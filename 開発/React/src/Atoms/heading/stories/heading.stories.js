71d9dcfb9eb542c9bc2201915f875af9aff08cb6
ryota-1231 <54019059+ryota-1231@users.noreply.github.com>
2020-03-10 20:19:07 +0900

    fix
    コメントアウト削除

--- 開発/React/src/Atoms/heading/stories/heading.stories.js
+++ 開発/React/src/Atoms/heading/stories/heading.stories.js
@@ -20,27 +20,3 @@ const H3 = styled.h3`
 h3Tag.story = {
   name: 'h3'
 }
-
-// import React from 'react';
-
-// import { action } from '@storybook/addon-actions';
-// import { Button } from '@storybook/react/demo';
-
-// export default {
-//   title: 'w',
-//   component: Button,
-// };
-
-// export const Text = () => <Button onClick={action('click')}>Hello Button</Button>;
-
-// export const Emoji = () => (
-//   <Button onClick={action('clicked')}>
-//     <span role="img" aria-label="so cool">
-//       😀 😎 👍 💯
-//     </span>
-//   </Button>
-// );
-
-// // Emoji.story = {
-// //   name: 'with emoji',
-// // };
