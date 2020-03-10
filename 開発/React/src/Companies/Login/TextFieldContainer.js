a4b7c52e37e8e8c2d666039d07aa0f916e9fef46
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-03-10 22:28:56 +0900

    standard --fix

--- 開発/React/src/Companies/Login/TextFieldContainer.js
+++ 開発/React/src/Companies/Login/TextFieldContainer.js
@@ -7,8 +7,8 @@ class TextFieldContainer extends React.Component {
     return (
       <div className='form-area'>
         <form className='text-form' noValidate autoComplete='off'>
-          <TextField id='Outlined-company-id' label='Company ID' variant="outlined" />
-          <TextField id='Outlined-password' label='PassWord' variant="outlined" />
+          <TextField id='Outlined-company-id' label='Company ID' variant='outlined' />
+          <TextField id='Outlined-password' label='PassWord' variant='outlined' />
         </form>
       </div>
     )
--- 開発/React/src/Items/complete/button.js
+++ 開発/React/src/Items/complete/button.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
+import React from 'react'
+import Button from '@material-ui/core/Button'
 
-export default function ContainedButtons() {
-
+export default function ContainedButtons () {
   return (
     <div>
-      <p className="button-block"><Button href="#" variant="contained" color="primary" className="btn">続けて出品</Button></p>
-      <p className="button-block"><Button href="#" variant="contained" color="primary" className="btn">シェアする</Button></p>
-      <p><Button href="#text-buttons" color="primary">ホームへ</Button></p>
+      <p className='button-block'><Button href='#' variant='contained' color='primary' className='btn'>続けて出品</Button></p>
+      <p className='button-block'><Button href='#' variant='contained' color='primary' className='btn'>シェアする</Button></p>
+      <p><Button href='#text-buttons' color='primary'>ホームへ</Button></p>
     </div>
-  );
+  )
 }
