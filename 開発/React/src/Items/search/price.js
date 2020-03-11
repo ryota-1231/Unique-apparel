8832cfda8ea74892d9d2c1695ba1025c71626def
sunadoi <y.sunadoi@gmail.com>
2020-03-11 13:25:59 +0900

    Create price.js

---
+++ 開発/React/src/Items/search/price.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Slider from '@material-ui/core/Slider';
+
+const useStyles = makeStyles({
+  root: {
+    width: 200,
+  },
+});
+
+export default function PriceRange() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState([0, 2000]);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root} id="price-range">
+      <Slider
+        value={value}
+        max="2000"
+        step="100"
+        onChange={handleChange}
+        valueLabelDisplay="auto"
+        aria-labelledby="range-slider"
+      />
+    </div>
+  );
+}
