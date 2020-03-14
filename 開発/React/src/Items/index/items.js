e8132063ad2656d4d182654a387dcbc9df26602e
sunadoi <y.sunadoi@gmail.com>
2020-03-14 16:18:58 +0900

    Update items.js

--- 開発/React/src/Items/index/items.js
+++ 開発/React/src/Items/index/items.js
@@ -1,27 +1,15 @@
 import React from 'react'
 import Link from '@material-ui/core/Link'
 
-export default function Items () {
-  // 最終的にはDBからitems.allとかで持ってくる予定
-  const items = [
-    { name: '商品1', price: '1000', image: '/static/images/購入完了画面.png' },
-    { name: '商品2', price: '2000', image: '/static/images/購入完了画面.png' },
-    { name: '商品3', price: '3000', image: '/static/images/購入完了画面.png' },
-    { name: '商品4', price: '4000', image: '/static/images/購入完了画面.png' },
-    { name: '商品1', price: '1000', image: '/static/images/購入完了画面.png' },
-    { name: '商品2', price: '2000', image: '/static/images/購入完了画面.png' },
-    { name: '商品3', price: '3000', image: '/static/images/購入完了画面.png' },
-    { name: '商品4', price: '4000', image: '/static/images/購入完了画面.png' },
-    { name: '商品1', price: '1000', image: '/static/images/購入完了画面.png' },
-    { name: '商品2', price: '2000', image: '/static/images/購入完了画面.png' },
-    { name: '商品3', price: '3000', image: '/static/images/購入完了画面.png' },
-    { name: '商品4', price: '4000', image: '/static/images/購入完了画面.png' }]
-  return (
-    <div className='items'>
-      {items.map((item) =>
-        <Item item={item} key={item.name + item.price} />)}
-    </div>
-  )
+class Items extends React.Component {
+  render () {
+    return (
+      <div className='items'>
+        {this.props.itemData.map((item) =>
+          <Item item={item} key={item.name + item.price} />)}
+      </div>
+    )
+  }
 }
 
 const Item = (props) => {
@@ -34,3 +22,5 @@ const Item = (props) => {
     </div>
   )
 }
+
+export default Items
