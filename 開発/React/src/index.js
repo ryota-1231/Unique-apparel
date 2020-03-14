75aee12feab9acf1e01081f23a5066b5594b6851
sunadoi <y.sunadoi@gmail.com>
2020-03-14 16:18:53 +0900

    Update index.js

--- 開発/React/src/index.js
+++ 開発/React/src/index.js
@@ -1,30 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import ItemSearch from './Items/search/main'
-// import ItemIndex from './Items/index/main'
+// import ItemSearch from './Items/search/main'
+import ItemIndex from './Items/index/main'
 import './reset.css'
 // import OrderComplete from './Orders/complete/main'
 // import UsersSignUp from './Users/SignUp/main'
 // import UsersLogin from './Users/Login/main'
 // import ListingNew from './Items/listings/main'
-import ListingEdit from './Items/listings/mainEdit'
+// import ListingEdit from './Items/listings/mainEdit'
 // import Purchase from './Items/purchase/main'
 // import ItemComplete from './Items/complete/main'
 // import CompaniesSignUp from './Pages/Companies/SignUp/main'
-import UsersMyPage from './Pages/Users/MyPage/main'
+// import UsersMyPage from './Pages/Users/MyPage/main'
 
 ReactDOM.render(
   // <UsersSignUp />,
 //   <UsersLogin />,
   // <OrderComplete />,
-    <ItemSearch />,
-  // <ItemIndex />,
+    // <ItemSearch />,
+  <ItemIndex />,
   // <ListingNew />,
   // <ListingEdit />,
   // <Purchase />,
   // <ItemComplete />,
   // <CompaniesSignUp />,
-  <UsersMyPage />,
+  // <UsersMyPage />,
   document.getElementById('root')
 )
