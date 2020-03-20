764838b283a0e9f504bb686bfe31ed0b0935acd2
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-03-20 17:51:28 +0900

    update itemDescription.js

--- 開発/React/src/Items/itemDetail/itemDescription.js
+++ 開発/React/src/Items/itemDetail/itemDescription.js
@@ -1,13 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
+import P from '../../Atoms/input/description'
 
 export default function ItemDescription () {
   return (
-    <section  className='item-description-container'>
-      <p>ここは商品の説明一行目です</p>
-      <p>ここは商品の説明二行目です</p>
-      <p>ここは商品の説明三行目です</p>
-      <p>ここは商品の説明四行目です</p>
-      <p>ここは商品の説明五行目です</p>
-    </section>
+    <Wrapper>
+      <StyledP description={['ここは商品の説明一行目です','ここは商品の説明二行目です', 'ここは商品の説明三行目です', 'ここは商品の説明四行目です', 'ここは商品の説明五行目です']} />
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  width: auto;
+  height: 400px;
+  margin: 20px 0;
+  border: 1px solid #eee;
+  border-radius: 4px;
+`
+
+const StyledP = styled(P)`
+padding: 20px 5px 0 5px;
+`
