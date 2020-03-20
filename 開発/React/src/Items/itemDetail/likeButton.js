4d2079b7c70c6431558fde98e865fef21ebacc5f
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-03-20 17:51:51 +0900

    update likeButton.js

--- 開発/React/src/Items/itemDetail/likeButton.js
+++ 開発/React/src/Items/itemDetail/likeButton.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
 
-export default function LikeButton () {
+export default function ButtonArea () {
   return (
-    <span className='button-container'>♡いいね！４</span>
+    <Wrapper>
+      <Button>♡いいね4</Button>
+      <Button>不適切な商品の報告</Button>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  width: 900px;
+  height: auto;
+  margin: 0 0 20px 0;
+`
+const Button = styled.button`
+  border: 1px solid #eee;
+  border-radius: 50px;
+  background: #eee;
+  height: 40px;
+  padding: 5px 15px;
+  line-height: 15px;
+  margin: 0 10px 0 0;
+  &:hover {
+    cursor: pointer;
+  }
+`
