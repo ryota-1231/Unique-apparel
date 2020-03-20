819e553e2557ccdaa5564c96c48ae85e170913e2
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-03-20 17:52:31 +0900

    update show.js

--- 開発/React/src/Items/show.js
+++ 開発/React/src/Items/show.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 import './itemDetail/show.css'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(fab, fas, far);
+import H1 from '../Atoms/heading/h1'
 import LeftImageArea from './itemDetail/leftImageArea.js'
 import RightDetailArea from './itemDetail/rightDetailArea.js'
 import ItemPrice from './itemDetail/itemPrice.js'
 import ItemDescription from './itemDetail/itemDescription.js'
-import LikeButton from './itemDetail/likeButton.js'
-import InappropriateButton from './itemDetail/inappropriateButton.js'
+import ButtonArea from './itemDetail/likeButton.js'
+import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
+import SocialButton from '../Items/itemDetail/socialButton'
+import OtherListingItem from '../Items/itemDetail/otherListingItem'
 
 class ItemDetail extends Component {
   render () {
@@ -14,22 +22,28 @@ class ItemDetail extends Component {
       <div className='contents'>
         {/* ここにロゴを配置する */}
         <div className='main-contents'>
-          <h1>一点ものにつき早いもの勝ち！メンズTシャツ</h1>
-          <article className='item-detail-container'>
+          {/* ①商品名 */}
+          <H1 title='お買い得商品！'></H1>
+          <section className='item-detail-container'>
+            {/* ②商品画像 */}
             <LeftImageArea />
+            {/* ③出品会社名④カテゴリー⑤商品サイズ */}
             <RightDetailArea />
-          </article>
-          <article className='item-price-container'>
-            <ItemPrice/>
-            <Button variant='contained' color='secondary' className='buying-button'>
-              購入画面に進む
-            </Button>
-          </article>
-          <article>
-            <ItemDescription/>
-            <LikeButton/>
-            <InappropriateButton/>
-          </article>
+          </section>
+          {/* ⑥商品の値段 */}
+          <ItemPrice/>
+          {/* ⑦購入画面遷移ボタン */}
+          <Button variant='contained' color='secondary' className='buying-button'>購入画面に進む</Button>
+          {/* ⑧商品説明 */}
+          <ItemDescription />
+          {/* ⑨いいねボタン */}
+          <ButtonArea />
+          {/* ⑩コメント */}
+          <TextField id='comment-input' label='コメント' fullWidth variant='outlined' />
+          {/* ⑪SNS */}
+          <SocialButton />
+          {/* ⑫出品会社のその他の商品・会社情報ページ遷移ボタン⑬その他の商品画像⑭その他の商品名⑮その他の商品額⑯その他商品のいいね */}
+          <OtherListingItem />
         </div>
       </div>
     )
