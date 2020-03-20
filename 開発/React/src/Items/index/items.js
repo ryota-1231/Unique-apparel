2ec4c5871b510e04eb9821be7fb843ca8b4d66e8
ryota-1231 <rrrrr.1231@gmail.com>
2020-03-20 16:46:11 +0900

    fix

--- 開発/React/src/Items/index/items.js
+++ 開発/React/src/Items/index/items.js
@@ -1,27 +1,35 @@
 import React from 'react'
-import Link from '@material-ui/core/Link'
+// import Link from '@material-ui/core/Link'
+import PropTypes from 'prop-types'
 
 class Items extends React.Component {
   render () {
     return (
       <div className='items'>
-        {this.props.itemData.map((item) =>
-          <Item item={item} key={item.id} />)}
+        {/* {this.props.itemData.map((item) =>
+          <Item item={item} key={item.id} />)} */}
       </div>
     )
   }
 }
 
-const Item = (props) => {
-  const { name, price, images } = props.item
-  const url = `/static/images/${images[0].image_url}`
-  return (
-    <div className='item'>
-      <Link href='#'><img src={ url } className='item-image' alt='アイコン' /></Link>
-      <p>{name}</p>
-      <p>¥{price}</p>
-    </div>
-  )
+// const Item = (props) => {
+//   const { name, price, images } = props.item
+//   const url = `/static/images/${images[0].image_url}`
+//   return (
+//     <div className='item'>
+//       <Link href='#'><img src={ url } className='item-image' alt='アイコン' /></Link>
+//       <p>{name}</p>
+//       <p>¥{price}</p>
+//     </div>
+//   )
+// }
+
+Items.propTypes = {
+  itemData: PropTypes.string,
+  item: PropTypes.object,
+  name:PropTypes.string,
+  props: PropTypes.object
 }
 
 export default Items
