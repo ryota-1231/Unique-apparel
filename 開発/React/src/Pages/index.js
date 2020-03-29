79cf9adf794ef79f651f6294cfb45b5531154c56
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-03-29 13:18:58 +0900

    update index.js

--- 開発/React/src/Pages/index.js
+++ 開発/React/src/Pages/index.js
@@ -8,6 +8,8 @@ import UsersMyPage from '../Pages/Users/MyPage/main'
 import CompanyLogin from '../Pages/Companies/Login/main'
 import CompanySignUp from '../Pages/Companies/SignUp/main'
 import ItemIndex from '../Pages/Items/index/main'
+import ItemShow from './Items/Detail/main'
+import CompanyMypage from './Companies/mypage/main'
 
 
 export default function index () {
@@ -23,7 +25,8 @@ export default function index () {
               <li><RouteLink to="/companies/login">CompanyLogin</RouteLink></li>
               <li><RouteLink to="/companies/signup">CompanySignUp</RouteLink></li>
               <li><RouteLink to="/items/index">ItemIndex</RouteLink></li>
-
+              <li><RouteLink to="/items/show/:id">ItemShow</RouteLink></li>
+              <li><RouteLink to="/companies/mypage">CompanyMypage</RouteLink></li>
             </ul>
           </div>
           <Switch>
@@ -34,6 +37,8 @@ export default function index () {
             <Route exact path='/companies/login' component={CompanyLogin}></Route>
             <Route exact path='/companies/signup' component={CompanySignUp}></Route>
             <Route exact path='/items/index' component={ItemIndex}></Route>
+            <Route path='/items/show/:id' component={ItemShow}></Route>
+            <Route exact path='/companies/mypage' component={CompanyMypage}></Route>
             {/* <Route exact path='/page3' component={CompanyLogin}></Route> */}
             <Route component={TopPage}></Route>
           </Switch>
